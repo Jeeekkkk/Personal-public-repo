ce2a86ee544e55975d8715e011d3931648b37707
Jeeekkkk <jcdalton34@gmail.com>
2025-01-13 13:26:30 +1100

    Erasing history- enabling to public

---
+++ Home-Assistant-Automations/pushupdates.sh
@@ -0,0 +1,6 @@
+cd /config
+git add .
+git config --global user.name "Jeeekkkk"
+git config --global user.email "jcdalton34@gmail.com"
+git commit -m "Automation script captured updates at `date +'%Y-%m-%d %H:%M:%S'`"
+git push -u origin main
